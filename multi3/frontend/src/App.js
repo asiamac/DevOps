44a5fbb58539355c94709c0741d144306ea62084
asiamac <asia.mackowiak@gmail.com>
2020-05-10 00:11:42 +0200

    lab6: add multicontainer app calculating horizontal projectiles

---
+++ multi3/frontend/src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './App.css';
+import HorizontalDistanceTravel from './HorizontalDistanceTravel';
+
+function App() {
+  return (
+    <div className='App'>
+      <HorizontalDistanceTravel />
+    </div>
+  )
+}
+
+export default App;
